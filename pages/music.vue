164570d856059f9c0dcc95b1668f22cd1a1b3741
Reinier <reinierburgering@gmail.com>
2023-03-10 21:23:51 +0100

    slight optimizations

--- pages/music.vue
+++ pages/music.vue
@@ -1,32 +1,33 @@
 <template>
   <div class="bg-black min-h-screen">
     <div class="w-3/4 mx-auto">
-      <div class="w-3/4 mx-auto py-16 flex">
-        <input type="text" class="border w-full p-4 rounded-3xl text-3xl" name="search" v-model="currentArtistSearch" placeholder="Search by artist" />
-        <button @click="handleSearch" class="bg-red-400 p-2 px-12 text-white -ml-16 rounded-r-3xl font-light">Search</button>
+      <div class="w-full sm:w-3/4 mx-auto py-16 sm:flex">
+        <input type="text" class="border w-full p-2 sm:p-4 rounded-3xl text-center sm:text-left text-base sm:text-3xl" name="search" v-model="currentArtistSearch" placeholder="Search by artist" />
+        <button @click="handleSearch" class="w-full sm:w-auto mt-4 sm:mt-0 bg-red-400 p-2 sm:py-5 px-12 text-white sm:-ml-16 sm:rounded-r-3xl font-light">Search</button>
       </div>
       <div v-if="currentArtist">
-        <div class="grid grid-cols-3 bg-blue-800" v-for="artist in currentArtist.artists.items" :key="artist.id">
+        <div class="sm:grid grid-cols-3" v-for="artist in currentArtist.artists.items" :key="artist.id">
           <div>
             <div class="text-white"></div>
-            <img :src="getImage(0)" alt="" />
           </div>
           <div class="text-white text-center">
-            <p class="text-3xl">{{ artist.name }}</p>
-            <ul v-if="artist.genres" class="flex justify-center">
-              <li v-for="(genre, index) in artist.genres" :key="index" class="mx-2 italic text-sm my-4">
-                {{ genre }}
-              </li>
-            </ul>
-            <p>Popularity: {{ artist.popularity }}</p>
-          </div>
-          <div class="text-white">
+            <img :src="getImage(1)" alt="" class="mx-auto"/>
+            <div class="my-6">
+              <p class="text-3xl">{{ artist.name }}</p>
+              <ul v-if="artist.genres" class="flex justify-center">
+                <li v-for="(genre, index) in artist.genres" :key="index" class="mx-2 italic text-sm my-4">
+                  {{ genre }}
+                </li>
+              </ul>
+              <p>Popularity: {{ artist.popularity }}</p>
+            </div>
+
             <ul v-if="currentTopTracks">
               <li v-for="(track, index) in currentTopTracks.tracks" :key="index" class="flex mb-3 bg-gray-900 min-h-[90px]">
                 <div class="w-2/6">
                   <img class="object-cover bg-red-300 h-full" :src="track.album.images[2].url" :alt="`Image of the album ${track.album.name}`" />
                 </div>
-                <div class="w-5/6 py-1">
+                <div class="w-5/6 py-1 flex items-start flex-col">
                   <span class="text-xl">{{ track.name }}</span
                   ><br />
                   <span class="text-sm italic">{{ track.album.name }} </span>
@@ -34,6 +35,7 @@
               </li>
             </ul>
           </div>
+          <div class="text-white"></div>
         </div>
       </div>
     </div>
